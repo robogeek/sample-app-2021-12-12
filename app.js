3a8f0d885f4f2534fc426d57979daf6f8a9e6a30
David Herron <david@davidherron.com>
2021-12-13 17:51:57 -0800

    Exercise the square_root function

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 const P = require('package');
 
 P.hello('There');
+
+console.log(`20: ${P.square_root(20)}`);
+console.log(`0: ${P.square_root(0)}`);
+console.log(`-20: ${P.square_root(-20)}`);
+console.log(`twenty: ${P.square_root('twenty')}`);
